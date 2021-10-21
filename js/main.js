7fd5100393d5058f8e65ab4c3f911b9d0760ff48
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-10-21 16:57:10 +0900

    feat: メニューの実装(#3)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 import '../css/style.css';
+import 'wicg-inert';
+
+const heroHeader = document.getElementById('hero-header');
+
+const openBtn = document.getElementById('open-menu-btn');
+openBtn.addEventListener('click', () => {
+  document.getElementById('menu').classList.add('menu__open');
+  heroHeader.inert = true;
+});
+
+const closeBtn = document.getElementById('close-menu-btn');
+closeBtn.addEventListener('click', () => {
+  document.getElementById('menu').classList.remove('menu__open');
+  heroHeader.inert = false;
+});
