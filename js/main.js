c5b642e9bcd58b0797639f9c965c6457683baf63
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-10-22 17:56:10 +0900

    feat: リンクホバー時の下線アニメーション追加、inert調整(#3)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 import '../css/style.css';
 import 'wicg-inert';
 
+const topHeader = document.getElementById('top-header');
 const heroHeader = document.getElementById('hero-header');
+const footer = document.getElementById('footer');
 
 const openBtn = document.getElementById('open-menu-btn');
 openBtn.addEventListener('click', () => {
   document.getElementById('menu').classList.add('menu__open');
-  heroHeader.inert = true;
+  topHeader.setAttribute('inert', true);
+  heroHeader.setAttribute('inert', true);
+  footer.setAttribute('inert', true);
 });
 
 const closeBtn = document.getElementById('close-menu-btn');
 closeBtn.addEventListener('click', () => {
   document.getElementById('menu').classList.remove('menu__open');
-  heroHeader.inert = false;
+  topHeader.removeAttribute('inert', false);
+  heroHeader.removeAttribute('inert', false);
+  footer.removeAttribute('inert', false);
 });
